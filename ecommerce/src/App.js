4b787f262b0d2420ff26f2517ab7a4b421af2757
OilPainter1 <hjcryns@gmail.com>
2023-02-13 19:16:10 -0600

    finished table block component

---
+++ ecommerce/src/App.js
@@ -0,0 +1,16 @@
+import TableBlock from './components/tableBlock';
+
+function App() {
+  return (
+    <div className="App">
+    <TableBlock 
+      name="test"
+      sku="1"
+      image="#">
+
+      </TableBlock>
+    </div>
+  );
+}
+
+export default App;
