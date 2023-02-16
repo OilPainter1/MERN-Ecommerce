f144874b34dcd4e16f29d44d83a317626f78c801
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-02-15 21:23:39 -0600

    header

---
+++ ecommerce/src/components/footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <footer className="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025)">eCommerce 2023</footer>
+    )
+}
+
+export default Footer
