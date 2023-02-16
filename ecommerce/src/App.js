f1ef5c191a9ad8c255e870514433b33a2624e9da
OilPainter1 <hjcryns@gmail.com>
2023-02-15 19:34:42 -0600

    dynamic render of components on add-to-table button click

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,48 +1,59 @@
 import TableBlock from './components/tableBlock';
 import {useState} from 'react'
 import SearchBar from './components/searchBar';
+import AddToTableButton from './components/addToTableButton';
 
 
 function App() {
   
   const[isFirstTableRow] = useState(true)
-
-  return (
+  const [view,setView] = useState("home")
+  if (view === "home"){
+    return (
     
-    <div className="App">
-      <SearchBar></SearchBar>
-
-      <TableBlock 
-        isFirstTableRow={isFirstTableRow}
-        name="test1"
-        sku="1"
-        image="#1"
-        price = "10">
-      </TableBlock>
-
-      <TableBlock  
-        name="test2"
-        sku="2"
-        image="#2"
-        price = "40">
-      </TableBlock>
-
-      <TableBlock 
-        name="test3"
-        sku="3"
-        image="#3"
-        price = "15">
-      </TableBlock>
-
-      <TableBlock
-        name="test4"
-        sku="4"
-        image="#4"
-        price= "30">    
-      </TableBlock>
-
-    </div>
-  );
+      <div className="App">
+        <SearchBar></SearchBar>
+  
+        <TableBlock 
+          isFirstTableRow={isFirstTableRow}
+          name="test1"
+          sku="1"
+          image="#1"
+          price = "10">
+        </TableBlock>
+  
+        <TableBlock  
+          name="test2"
+          sku="2"
+          image="#2"
+          price = "40">
+        </TableBlock>
+  
+        <TableBlock 
+          name="test3"
+          sku="3"
+          image="#3"
+          price = "15">
+        </TableBlock>
+  
+        <TableBlock
+          name="test4"
+          sku="4"
+          image="#4"
+          price= "30">    
+        </TableBlock>
+        
+        <AddToTableButton view={view} setView={setView}></AddToTableButton>
+      </div>
+)} else
+      if (view === "AddTableRow"){
+        return(
+        <>
+        <h2>
+          add to table
+        </h2></>)
+      }
+  
 }
 
 export default App;
