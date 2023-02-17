2ad66e323e4700e57adcc004c23ff262ac068b1b
omen <kybar089@gmail.com>
2023-02-16 19:36:17 -0600

    updated mutations.js and queries.js

--- ecommerce/src/utils/queries.js
+++ ecommerce/src/utils/queries.js
@@ -10,29 +10,20 @@ export const QUERY_USER = gql`
   }
 `;
 
-export const QUERY_THOUGHTS = gql`
-  query getThoughts {
-    thoughts {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-    }
+export const QUERY_ALL_PRODUCTS = gql`
+{
+  products {
+    _id
+    name
+    image
+    brand
+    sizes
+    category
+    style
+    color 
+    price
+    description
+    quantityInStock
   }
-`;
-
-export const QUERY_SINGLE_THOUGHT = gql`
-  query getSingleThought($thoughtId: ID!) {
-    thought(thoughtId: $thoughtId) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        createdAt
-      }
-    }
-  }
+}
 `;
