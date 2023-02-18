f8def26b9b840732756088398484c60ff054f6e7
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-02-18 13:49:34 -0600

    addProductForm container/color/border

--- ecommerce/src/views/AddProductForm.js
+++ ecommerce/src/views/AddProductForm.js
@@ -1,6 +1,7 @@
 
 import { useMutation } from '@apollo/client'; 
 import { ADD_PRODUCT } from '../utils/mutations';
+import Footer from '../components/footer';
 
 
 
@@ -33,7 +34,7 @@ function AddProductForm(props) {
     
     return (
     <>
-        <h2>Add New Product</h2>
+        <h2 className='container'>Add New Product</h2>
         <form className="container prodForm" onSubmit={(event) =>{
                 event.preventDefault()
                 console.log(style,stock)
