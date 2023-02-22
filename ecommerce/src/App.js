7ee4085f3b27d422106e8d6fc70a87f32b1a63c9
omen <kybar089@gmail.com>
2023-02-22 13:29:59 -0600

    Seeded images now rendering

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,5 +1,5 @@
 import TableBlock from './components/tableBlock';
-import {useState} from 'react'
+import { useState } from 'react'
 import SearchBar from './components/searchBar';
 import Header from './components/header';
 import Footer from './components/footer';
@@ -9,59 +9,57 @@ import { QUERY_ALL_PRODUCTS } from './utils/queries';
 import { useQuery } from '@apollo/client'
 
 
-
-
 function App() {
   const [searchTerm, setSearchTerm] = useState("")
-  const[isFirstTableRow] = useState(true)
-  const [view,setView] = useState("home")
+  const [isFirstTableRow] = useState(true)
+  const [view, setView] = useState("home")
   console.log(searchTerm)
-  const { data, loading, error } =  useQuery(QUERY_ALL_PRODUCTS,{
+  const { data, loading, error } = useQuery(QUERY_ALL_PRODUCTS, {
     variables: {
       searchTerm
     }
-  } )
+  })
   console.log(data, loading, error)
 
   const products = data?.products || []
 
+  if (view === "home") {
 
-
-  if (view === "home"){
     return (
-    
 
-    <div className="App">
-
-      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      <div className="App">
+        <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <div className='container prodForm'>
           {products.map(product => {
+
             return (
-              <TableBlock 
+
+              <TableBlock
                 isFirstTableRow={isFirstTableRow}
-                name= {product.name}
-                sku= {product._id}
-                image= {product.image}
-                price = {product.price}
-                quantityInStock= {product.quantityInStock}>
+                name={product.name}
+                sku={product._id}
+                image={<img src={product.image} width={200} height={200} alt={product.name} />}
+                // image= {<img src={`./assets/${image}`} />}
+                price={product.price}
+                quantityInStock={product.quantityInStock}>
               </TableBlock>
             )
           })}
-        <AddToTableButton view={view} setView={setView}></AddToTableButton>
+          <AddToTableButton view={view} setView={setView}></AddToTableButton>
         </div>
         <Footer />
       </div>
-      
-)} else
-      if (view === "AddTableRow"){
-        return(
+    )
+  } else
+    if (view === "AddTableRow") {
+
+      return (
+
         <>
           <AddProductForm view={view} setView={setView}></AddProductForm>
-        
         </>
-        
-       )
-      }
+      )
+    }
 
 }
 
