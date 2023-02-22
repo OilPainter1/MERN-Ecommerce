081d64b36fb0938dd45239fbe414d3d4cddb69dc
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2023-02-22 13:54:56 -0600

    modal in header

--- ecommerce/src/components/header.js
+++ ecommerce/src/components/header.js
@@ -1,7 +1,13 @@
 import Auth from '../utils/auth'
 import SearchBar from './searchBar'
+import { Modal } from 'react-bootstrap'
+import { useState } from 'react'
+
 
 const Header = ({searchTerm, setSearchTerm}) => {
+
+  const [showModal, setShowModal] = useState(false);
+
     return (
         <nav className="navbar bg-body-tertiary" id="header">
           <div className="container-fluid">
@@ -16,6 +22,12 @@ const Header = ({searchTerm, setSearchTerm}) => {
                 >Login/Sign Up</Nav.Link>)} */}
 
             <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
+            <Modal
+        size='lg'
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby='signup-modal'></Modal>
         </div>
       </nav>
     )
