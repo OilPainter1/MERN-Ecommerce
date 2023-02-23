aab38e0c2bf32734d5e8593f6a36286d0e62e1d3
OilPainter1 <hjcryns@gmail.com>
2023-02-22 21:14:49 -0600

    fixed bugs

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,65 +1,63 @@
-import TableBlock from './components/tableBlock';
-import { useState } from 'react'
-import Header from './components/header';
-import Footer from './components/footer';
-import AddToTableButton from './components/addToTableButton';
-import AddProductForm from './views/AddProductForm';
-import { QUERY_ALL_PRODUCTS } from './utils/queries';
-import { useQuery } from '@apollo/client'
-
+import TableBlock from "./components/tableBlock";
+import { useState } from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import AddToTableButton from "./components/addToTableButton";
+import AddProductForm from "./views/AddProductForm";
+import { QUERY_ALL_PRODUCTS } from "./utils/queries";
+import { useQuery } from "@apollo/client";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("")
-  const [view, setView] = useState("home")
+  const [searchTerm, setSearchTerm] = useState("");
+  const [view, setView] = useState("home");
   const { data, loading, error } = useQuery(QUERY_ALL_PRODUCTS, {
     variables: {
-      searchTerm
-    }
-  })
-  console.log(data, loading, error)
+      searchTerm,
+    },
+  });
+  console.log(data, loading, error);
 
-  const products = data?.products || []
+  const products = data?.products || [];
 
   if (view === "home") {
-
     return (
-
       <div className="App">
         <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <div className='container prodForm'>
-          {products.map(product => {
-
+        <div className="container prodForm">
+          {products.map((product) => {
             return (
               <TableBlock
                 key={product._id}
-                
-                image={<img src={product.image} width={200} height={200} alt={product.name} />}
+                image={
+                  <img
+                    src={product.image}
+                    width={200}
+                    height={200}
+                    alt={product.name}
+                  />
+                }
                 // image= {<img src={`./assets/${image}`} />}
                 sku={product._id}
                 brand={product.brand}
                 name={product.name}
                 color={product.color}
                 price={product.price}
-                quantityInStock={product.quantityInStock}>
-              </TableBlock>
-            )
+                quantityInStock={product.quantityInStock}
+              ></TableBlock>
+            );
           })}
           <AddToTableButton view={view} setView={setView}></AddToTableButton>
         </div>
         <Footer />
       </div>
-    )
-  } else
-    if (view === "AddTableRow") {
-
-      return (
-
-        <>
-          <AddProductForm view={view} setView={setView}></AddProductForm>
-        </>
-      )
-    }
-
+    );
+  } else if (view === "AddTableRow") {
+    return (
+      <>
+        <AddProductForm view={view} setView={setView}></AddProductForm>
+      </>
+    );
+  }
 }
 
 export default App;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,59 +1,57 @@
-const { gql } = require('apollo-server-express')
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        username: String!
-        email: String!
-        password: String!
-    }
+  type User {
+    username: String!
+    email: String!
+    password: String!
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
-    
-    type Product {
-        _id: ID!
-        name: String!
-        image: String!
-        brand: String! 
-        sizes: [Float]
-        category: [String]
-        style: String!
-        color: [String]
-        price: Float
-        description: String!
-        quantityInStock: Int
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    input SaveProductInput {
-        name: String!
-        image: String!
-        brand: String! 
-        sizes: [Float]
-        category: [String]
-        style: String!
-        color: [String]
-        price: Float
-        description: String!
-        quantityInStock: Int
-    }
+  type Product {
+    _id: ID!
+    name: String!
+    image: String!
+    brand: String!
+    sizes: [Float]
+    category: [String]
+    style: String!
+    color: [String]
+    price: Float
+    description: String!
+    quantityInStock: Int
+  }
 
-    type Query {
-        me: User
-        user(username: String!): User
-        products(searchTerm:String):[Product]
-    }
+  input SaveProductInput {
+    name: String!
+    image: String!
+    brand: String!
+    sizes: [Float]
+    category: [String]
+    style: String!
+    color: [String]
+    price: Float
+    description: String!
+    quantityInStock: Int
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveProduct(input: SaveProductInput): Product
-        removeProduct(productId: ID!): Product
-        addToStock(productId: ID!, stock: Int!): Product
-    }
+  type Query {
+    me: User
+    user(username: String!): User
+    products(searchTerm: String): [Product]
+  }
 
-  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveProduct(input: SaveProductInput): Product
+    removeProduct(productId: ID!): Product
+    addToStock(productId: ID!, stock: Int!): Product
+  }
 `;
 
 module.exports = typeDefs;
