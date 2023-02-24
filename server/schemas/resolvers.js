bf6097c837a364792dd1cee67334cdd6384aff45
omen <kybar089@gmail.com>
2023-02-23 18:57:11 -0600

    signup/login working agian

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -22,17 +22,21 @@ const resolvers = {
     },
   },
     Mutation: {
-        login: async (parent, { email, password }) => {
-            console.log(email, password)
-            const user = await User.findOne({ email });
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
 
+        if (!user) {
+            throw new AuthenticationError('No user with this email found!');
+        }
 
-      if (!correctPw) {
-        throw new AuthenticationError("Incorrect password!");
-      }
+        const correctPw = await user.isCorrectPassword(password);
 
-      const token = signToken(user);
-      return { token, user };
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect password!');
+        }
+  
+        const token = signToken(user);
+        return { token, user };
     },
 
     addToStock: async (parent, { productId, stock }) => {
